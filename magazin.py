1efb03b351a804c10eaa5e30939fa61e0df83f93
Student User <student@cs.pub.ro>
2014-06-27 12:35:47 +0300

    baza de date

--- magazin.py
+++ magazin.py
@@ -1,7 +1,14 @@
 import flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app=flask.Flask(__name__)
 app.config['SECRET_KEY']='ceva secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/student/osss-web/db.sqlite'
+db=SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String)
 
 @app.route('/')
 def home():
@@ -10,8 +17,12 @@ def home():
 @app.route('/save', methods=['POST'])
 def save():
     print "saving...", flask.request.form['name']
+    product = Product(name=flask.request.form['name'])
+    db.session.add(product)
+    db.session.commit()
     flask.flash("produs salvat")
     return flask.redirect('/')
 
+db.create_all()
 app.run(debug=True)
 
