04e57edfaee6edecdbd3d643c6270654c1b08c32
Student User <student@cs.pub.ro>
2014-06-27 12:02:39 +0300

    message added

--- magazin.py
+++ magazin.py
@@ -1,15 +1,17 @@
 import flask
 
 app=flask.Flask(__name__)
+app.config['SECRET_KEY']='ceva secret'
 
 @app.route('/')
 def home():
 	return flask.render_template('home.html')
 
-@app.route('/save')
+@app.route('/save', methods=['POST'])
 def save():
-    print "saving..."
-    return 'ok'
+    print "saving...", flask.request.form['name']
+    flask.flash("produs salvat")
+    return flask.redirect('/')
 
 app.run(debug=True)
 
